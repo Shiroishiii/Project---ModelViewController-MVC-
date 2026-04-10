b6d9789b4d7dc201e2580b153362aa706c46f834
JulioGaspareto <juliogaspareto0@gmail.com>
2026-04-09 22:10:45 -0300

    Criando paginas de cadastro e login,otas ja foram vinculadas tmb. Pagina de cadastro esta em processo enquanto a de Login nao saiu do lugar.

--- FrontEnd/listfy/src/App.tsx
+++ FrontEnd/listfy/src/App.tsx
@@ -1,23 +1,12 @@
 import './App.css'
-import { useState } from 'react';
+
 
 
 function App() {
-  const [isChecked, setIsChecked] = useState(false)
 
   return (
     <>
-      <div className="p-6">
-        <label className="container">
-          <input
-            type="checkbox"
-            checked={isChecked}
-            onChange={() => setIsChecked(!isChecked)}
-          />
-          <div className="checkmark"></div>
-          Check
-        </label>
-      </div>
+    
     </>
   )
 }
